785034383e8b573ed99a110f2390555fbabf0cd1
Bharath Kumar Chava <bharath.kumar.chava@oracle.com>
2026-02-28 18:46:00 -0800

    Update to version 3

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from Python Flask! This is Version 2 – CI/CD demo on Rancher Desktop.'
+    return 'Hello from Python Flask! This is Version 3 – added /health check.'
+
+@app.route('/health')
+def health():
+    return 'OK - app is healthy'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
