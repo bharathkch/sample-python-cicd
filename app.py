5ab711df763c890074df8b5b1c9c68337c0d060d
Bharath Kumar Chava <bharath.kumar.chava@oracle.com>
2026-02-28 18:44:25 -0800

    Update to version 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from Python Flask! This is Version 1 – CI/CD demo on Rancher Desktop.'
+    return 'Hello from Python Flask! This is Version 2 – CI/CD demo on Rancher Desktop.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
